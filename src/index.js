1297ac802fae10de700eba07d791022e3de058ba
Igor Lyatskiy <igor.lyatskiy@gmail.com>
2021-01-30 17:27:35 +0300

    feat: edited file index. js, all have been done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
+exports.min = function min(array) {
+    let min = Infinity;
+    if (typeof array == "undefined") return 0;
+    if (array.length > 0) {
+        let length = array.length;
+        for (let i = 0; i < length; i++) {
+            value = array.shift();
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    } else return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let max = -Infinity;
+    if (typeof array == "undefined") return 0;
+    if (array.length > 0) {
+        let length = array.length;
+        for (let i = 0; i < length; i++) {
+            value = array.shift();
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    } else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let sum = 0;
+    if (typeof array == "undefined") return 0;
+    if (array.length > 0) {
+        let length = array.length;
+        for (let i = 0; i < length; i++) {
+            value = array.shift();
+            sum += value;
+        }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        return sum / length;
+    } else return 0;
+};
